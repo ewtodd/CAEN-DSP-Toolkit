724d664a11c17c840f36711a42ad071e3b749c71
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-10-29 13:56:49 -0400

    trying to figure out why default template not being recognized

--- flake.nix
+++ flake.nix
@@ -3,57 +3,56 @@
 
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self, nixpkgs, system }:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
 
-        toolkit = pkgs.stdenv.mkDerivation {
-          pname = "dsp-toolkit";
-          version = "0.1";
+      toolkit = pkgs.stdenv.mkDerivation {
+        pname = "dsp-toolkit";
+        version = "0.1";
 
-          src = ./.;
+        src = ./.;
 
-          nativeBuildInputs = with pkgs; [ pkg-config ];
+        nativeBuildInputs = with pkgs; [ pkg-config ];
 
-          buildInputs = with pkgs; [ root gsl ];
+        buildInputs = with pkgs; [ root gsl ];
 
-          installPhase = ''
-            mkdir -p $out/{lib,include,share/macros}
+        installPhase = ''
+          mkdir -p $out/{lib,include,share/macros}
 
-            cp lib/*.so $out/lib/ 2>/dev/null || true
-            cp lib/*.a $out/lib/ 2>/dev/null || true
+          cp lib/*.so $out/lib/ 2>/dev/null || true
+          cp lib/*.a $out/lib/ 2>/dev/null || true
 
-            if [ -d include ] && [ -n "$(ls -A include/*.h 2>/dev/null)" ]; then
-              cp include/*.h $out/include/
-            fi
+          if [ -d include ] && [ -n "$(ls -A include/*.h 2>/dev/null)" ]; then
+            cp include/*.h $out/include/
+          fi
 
-            if [ -d macros ] && [ -n "$(ls -A macros/*.C 2>/dev/null)" ]; then
-              cp macros/*.C $out/share/macros/
-            fi
-          '';
-          shellHook = ''
-            echo "Digital Signal Processing Development Environment for CAEN digitizers"
-            echo "ROOT version: $(root-config --version)"
-            echo ""
+          if [ -d macros ] && [ -n "$(ls -A macros/*.C 2>/dev/null)" ]; then
+            cp macros/*.C $out/share/macros/
+          fi
+        '';
+        shellHook = ''
+          echo "Digital Signal Processing Development Environment for CAEN digitizers"
+          echo "ROOT version: $(root-config --version)"
+          echo ""
 
-            # For clang?
-            export ROOT_INCLUDE_PATH="$PWD/include:$ROOT_INCLUDE_PATH"
-            export LD_LIBRARY_PATH="$PWD/build:$LD_LIBRARY_PATH"
+          # For clang?
+          export ROOT_INCLUDE_PATH="$PWD/include:$ROOT_INCLUDE_PATH"
+          export LD_LIBRARY_PATH="$PWD/build:$LD_LIBRARY_PATH"
+        '';
+      };
+
+    in {
+      packages.default = toolkit;
+      templates = {
+        default = {
+          path = ./templates/default;
+          description = "Standard ROOT waveform analysis pipeline.";
+          welcomeText = ''
+            Run `nix develop` to enter the development environment.
           '';
         };
-
-      in {
-        packages.default = toolkit;
-        templates = {
-          default = {
-            path = ./templates/default;
-            description = "Standard ROOT waveform analysis pipeline.";
-            welcomeText = ''
-              Run `nix develop` to enter the development environment.
-            '';
-          };
-        };
-        default = self.templates.default;
-      });
+      };
+      default = self.templates.default;
+    };
 }
